044352798b8ac52b0c8fc45fbb17f6c079365cfc
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2023-03-06 17:36:28 +0000

    Reworked key loading to no longer require promises@

--- KeyLoader.cjs
+++ KeyLoader.cjs
@@ -1,37 +1,40 @@
 const fs = require('fs');
 const config = require('./config.json');
 
+let bot;
 module.exports = {
-    loadKeys(bot) {
-        return new Promise( async (resolve, reject) => {
-            console.log(config);
-            await getDataFromFile(config.KEY_PATHS.FOLDER + config.KEY_PATHS.ACTIVE).then(data => {
-                if (data.length < 1) return;
-                bot['keys']['active'] = data.split("\n");
+    loadKeys(botClient) {
+
+        botClient['keys'] = {}
+
+        let data = getDataFromFile(config.KEY_PATHS.FOLDER + config.KEY_PATHS.ACTIVE)
+        botClient.keys['active'] = []
+        if (data.length >= 1){
+            botClient.keys.active = data.split("\n");
+        }
+
+        data = getDataFromFile(config.KEY_PATHS.FOLDER + config.KEY_PATHS.GOT_KEY)
+        botClient.keys["gotKey"] = [];
+        if (data.length >= 1){
+            botClient.keys.gotKey = data.split("\n").map( inString => {
+                const fields = inString.split(" ");
+                return {
+                    id : fields[0],
+                    key : fields[1]
+                }
             });
-            await getDataFromFile(config.KEY_PATHS.FOLDER + config.KEY_PATHS.GOT_KEY).then(data => {
-                if (data.length < 1) return;
-                bot['keys']['gotKey'] = data.split("\n").map( inString => {
-                    const fields = inString.split(" ");
-                    return {
-                        id : fields[0],
-                        key : fields[1]
-                    }
-                });
-            });
-            resolve();
-        })
+        }
+        bot = botClient;
     },
     updateKeys(newUserId, key) {
         bot.keys.gotKey.push({
             id : newUserId,
             key : key
         });
-
-        fs.writeFile(config.KEY_PATHS.ACTIVE, bot.keys.active.join("\n"), err => {
+        fs.writeFile(config.KEY_PATHS.FOLDER + config.KEY_PATHS.ACTIVE, bot.keys.active.join("\n"), err => {
             if (err) console.error(err);
         });
-        fs.writeFile(config.KEY_PATHS.GOT_KEY, bot.keys.gotKey.map(user => {
+        fs.writeFile(config.KEY_PATHS.FOLDER + config.KEY_PATHS.GOT_KEY, bot.keys.gotKey.map(user => {
             return `${user.id} ${user.key}`;
         }).join("\n"), err => {
             if (err) console.error(err);
@@ -42,14 +45,5 @@ module.exports = {
 
 
 function getDataFromFile(filePath) {
-    return new Promise((resolve, reject) => {
-        fs.readFileSync(filePath, (data, err) => {
-            if (err) {
-                console.log(`Error loading: ${filePath}`);
-                console.log(err);
-                resolve("");
-            };
-            resolve(data);
-        })
-    })
+    return fs.readFileSync(filePath).toString().replace(/\r/g, '');
 }
