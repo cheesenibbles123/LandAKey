0a7b26b29a2f69536e7fd061ad02fa2ed33527cf
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2023-03-06 16:14:32 +0000

    Modified module to export individual functions

--- KeyLoader.js
+++ KeyLoader.js
@@ -1,43 +1,42 @@
 import * as fs from 'fs';
 import * as config from './config.json' assert { type: "json" };
 
-module.exports = {
-    async loadKeys(bot) {
-        return new Promise( async (resolve, reject) => {
-            await getDataFromFile(config.KEY_PATH.FOLDER + config.KEY_PATH.ACTIVE).then(data => {
-                if (data.length < 1) return;
-                bot['keys']['active'] = data.split("\n");
+export function loadKeys(bot) {
+    return new Promise( async (resolve, reject) => {
+        await getDataFromFile(config.KEY_PATH.FOLDER + config.KEY_PATH.ACTIVE).then(data => {
+            if (data.length < 1) return;
+            bot['keys']['active'] = data.split("\n");
+        });
+        await getDataFromFile(config.KEY_PATH.FOLDER + config.KEY_PATH.GOT_KEY).then(data => {
+            if (data.length < 1) return;
+            bot['keys']['gotKey'] = data.split("\n").map( inString => {
+                const fields = inString.split(" ");
+                return {
+                    id : fields[0],
+                    key : fields[1]
+                }
             });
-            await getDataFromFile(config.KEY_PATH.FOLDER + config.KEY_PATH.GOT_KEY).then(data => {
-                if (data.length < 1) return;
-                bot['keys']['gotKey'] = data.split("\n").map( inString => {
-                    const fields = inString.split(" ");
-                    return {
-                        id : fields[0],
-                        key : fields[1]
-                    }
-                });
-            });
-            resolve();
-        })
-    },
-    updateKeys(newUserId, key) {
-        bot.keys.gotKey.push({
-            id : newUserId,
-            key : key
         });
-
-        fs.writeFile(config.KEY_PATH.ACTIVE, bot.keys.active.join("\n"), err => {
-            if (err) console.error(err);
-        });
-        fs.writeFile(config.KEY_PATH.GOT_KEY, bot.keys.gotKey.map(user => {
-            return `${user.id} ${user.key}`;
-        }).join("\n"), err => {
-            if (err) console.error(err);
-        });
-
-    }
+        resolve();
+    })
 }
+export function updateKeys(newUserId, key) {
+    bot.keys.gotKey.push({
+        id : newUserId,
+        key : key
+    });
+
+    fs.writeFile(config.KEY_PATH.ACTIVE, bot.keys.active.join("\n"), err => {
+        if (err) console.error(err);
+    });
+    fs.writeFile(config.KEY_PATH.GOT_KEY, bot.keys.gotKey.map(user => {
+        return `${user.id} ${user.key}`;
+    }).join("\n"), err => {
+        if (err) console.error(err);
+    });
+
+}
+
 
 function getDataFromFile(filePath) {
     return new Promise((resolve, reject) => {
